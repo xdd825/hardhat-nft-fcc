825c5103ca96051f7729088341c1194456cfaf27
Ben Force <xdd741852963@gmail.com>
2022-06-19 21:05:58 +0800

    add tokenURI function

---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,70 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+contract DynamicSvgNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix =
+        "data:image/svg+xml;base64,";
+
+    constructor(string memory lowSvg, string memory highSvg)
+        ERC721("Dynamic Nft Svg", "DNS")
+    {
+        s_tokenCounter = 0;
+    }
+
+    function storeImageURI(string memory svg)
+        public
+        pure
+        returns (string memory)
+    {
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return
+            string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(_exists(tokenId), "URI Query for nonexistent token");
+        string memory imageURI = "hi!";
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description":"An NFT that changes based on the Chainklink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+}
